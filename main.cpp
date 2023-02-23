aad817d51b6eeaf54139f6419b58a86495878206
GlebRTA <104761917+GlebRTA@users.noreply.github.com>
2023-02-23 21:42:26 +0200

    add Shell and quick sort

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ void printArray(int* arr, int n) {
 }
 
 void sortByBubble(int* arr, int n) {
-
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -23,7 +22,6 @@ void sortByBubble(int* arr, int n) {
 }
 
 void sortBySelection(int* arr, int n) {
-
     for (int i = 0; i < n - 1; i++) {
         int min = i;
         for (int j = i + 1; j < n; j++) {
@@ -39,7 +37,6 @@ void sortBySelection(int* arr, int n) {
 }
 
 void sortByInsert(int* arr, int n) {
-
     for (int i = 1; i < n; i++) {
         for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
             int temp = arr[j];
@@ -49,20 +46,65 @@ void sortByInsert(int* arr, int n) {
     }
 }
 
+void sortByShell(int* arr, int n) {
+    for (int dif = n / 2; dif > 0; dif /= 2) {
+        for (int i = dif; i < n; i++) {
+            for (int j = i; j >= dif && arr[j - dif] > arr[j]; j -= dif) {
+                int temp = arr[j];
+                arr[j] = arr[j - dif];
+                arr[j - dif] = temp;
+            }
+        }
+    }
+}
+
+int partOfSort(int* arr, int start, int end) {
+    int mid = arr[(start + end) / 2];
+    int i = start;
+    int j = end;
+    while (i <= j) {
+        while (arr[i] < mid) {
+            i++;
+        }
+        while (arr[j] > mid) {
+            j--;
+        }
+        if (i >= j) {
+            break;
+        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+    return j;
+}
+
+void quickSort(int* arr, int start, int end) {
+    if (start < end) {
+        int temp = partOfSort(arr, start, end);
+        quickSort(arr, 0, temp);
+        quickSort(arr, temp + 1, end);
+    }
+}
+
 void printSortTypes() {
     cout << "Choose type of sorting array: " << endl;
     cout << "Input 1 to bubble sort" << endl;
     cout << "Input 2 to selection sort" << endl;
     cout << "Input 3 to insert sort" << endl;
+    cout << "Input 4 to Shell sort" << endl;
+    cout << "Input 5 to quick sort" << endl;
 }
 
 
 int main() {
-    int n;
+    int size;
     cout << "Input array length" << endl;
-    cin >> n;
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++) {
+    cin >> size;
+    int* arr = new int[size];
+    for (int i = 0; i < size; i++) {
         cout << "Input " << i + 1 << ". array element" << endl;
         cin >> arr[i];
     }
@@ -71,23 +113,30 @@ int main() {
     cin >> type;
 
     cout << "Array before sorting: " << endl;
-    printArray(arr, 5);
+    printArray(arr, size);
+
     switch (type) {
         case 1:
-            sortByBubble(arr, 5);
+            sortByBubble(arr, size);
             break;
         case 2:
-            sortBySelection(arr, 5);
+            sortBySelection(arr, size);
             break;
         case 3:
-            sortByInsert(arr, 5);
+            sortByInsert(arr, size);
+            break;
+        case 4:
+            sortByShell(arr, size);
+            break;
+        case 5:
+            quickSort(arr, 0, size - 1);
             break;
         default:
             cout << "Incorrect input type" << endl;
     }
 
     cout << "Array after sorting: " << endl;
-    printArray(arr, 5);
+    printArray(arr, size);
 
     delete [] arr;
     return 0;
